7dd9b0a32c025d72185fea4d42607985c823eb81
Aidin <aidin.forooghi@gmail.com>
2023-04-11 23:00:57 -0400

    Passed 2 Test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    var dodger = document.getElementById('dodger');
+    var currentPosition = parseInt(dodger.style.left) || 0;
+    var newPosition = currentPosition - 10; // Move 10 pixels to the left
+    dodger.style.left = newPosition + 'px';
+}
+
+function moveDodgerRight() {
+    var dodger = document.getElementById('dodger');
+    var currentPosition = parseInt(dodger.style.left) || 0;
+    var newPosition = currentPosition + 10; // Move 10 pixels to the right
+    dodger.style.left = newPosition + 'px';
+}
